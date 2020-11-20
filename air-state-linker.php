f5b5f76e93bebb152ec615e096bbec6735b480b9
Vladimir Terziev <vladotoka@gmail.com>
2020-11-20 13:45:32 +0200

    first commit

---
+++ air-state-linker.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+Plugin Name: Air-state linker
+Plugin URI: https://github.com/vladotoka/project-ASL
+Description: Custom WP plugin which handling link between Buddypress and map MAPsvg plugins
+Version: 1.0.0
+Author: Vladimir Terziev
+Autor URI:
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: air-state-linker
+*/
+
+/* !0.TABLE OF CONTENTS
+
+    1.HOOKS
+
+    2.SHORTCODES
+
+    3.FILTERS
+
+    4.EXTERNAL SCRIPTS
+
+    5.ACTIONS
+
+    6.HELPERS
+
+    7.CUSTOM POST TYPES
+
+    8.ADMIN PAGES
+
+    9.SETTINGS
+*/
+
+/* !1.HOOKS */
+
+/* !2.SHORTCODES */
+
+/* !3.FILTERS */
+
+/* !4.EXTERNAL SCRIPTS */
+
+/* !5.ACTIONS */
+
+/* !6.HELPERS */
+
+/* !7.CUSTOM POST TYPES */
+
+/* !8.ADMIN PAGES */
+
+/* !9.SETTINGS */
---
+++ info.php
@@ -0,0 +1,30 @@
+/*
+1.Task
+Link MAPsvg plugin with custom developed 4 fields which indicates users from buddupress: 
+countries, developers, real estates, trading platforms
+
+2.Solution
+To write a plugin with function which take region as param and returns number of users total 
+and number of users from 3 types.
+Call this function on map loading and on map region click
+Return updated field to UI
+
+3.Plugin features
+PHP function asl_user_count
+
+Short code for UI data
+
+Ajax form handler?
+
+4.How will i accomplish this
+-PHP function asl_user_count
+>>researsh bp 
+
+-Short code for UI data
+
+Ajax form handler?
+
+info: Custom WP plugin wich handling link between WPDB and map MAPsvg plugin
+*/
+
+
